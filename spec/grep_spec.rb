2739099d3d6dd63da96d1f4df37fe05878f53f3d
Tam <tamborinehands@gmail.com>
2016-10-11 11:49:09 +0100

    deleted all but readme

--- spec/grep_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'grep'
-
-describe Grep do
-
-  it "searches filenames and matches the class name param" do
-    class_name = "Animal"
-    expect(subject.spec_file_exists?(class_name)).to eq false
-  end
-end
